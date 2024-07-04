f30b27c9e806c4e529f72d17c3ce310957a74764
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-07-04 20:53:56 +0530

    updated image process

--- YOLOv8/main.py
+++ YOLOv8/main.py
@@ -13,11 +13,26 @@ def show_img_from_path(path):
     cv2.imshow("original_image" , cv2.imread(path))
     cv2.waitKey()
 
-def show_head_predicted_image(original_image_path, head_cordinates):
+def show_head_predicted_image(original_image_path, cordinates):
     original_image = cv2.imread(original_image_path)
-    predicted_image = cv2.rectangle(original_image,  (int(head[0]), int(head[1])), (int(head[2]), int(head[3])), (0,255,0), 1)
-    cv2.imshow("predicted head image" , predicted_image)
+    predicted_image = cv2.rectangle(original_image,  (int(cordinates[0]), int(cordinates[1])), (int(cordinates[2]), int(cordinates[3])), (0,255,0), 1)
+    cv2.imshow("predicted image" , predicted_image)
     cv2.waitKey()
+
+def display_head_body(original_image_path, result):
+    # getting the head and body vector
+    
+    for i in result[0].names:
+        original_image = cv2.imread(original_image_path)
+        cv2.imshow("original_image_1", original_image)
+        # print(i, result[0].names[i])
+        print(result[0].boxes.xyxy[i], f"its the { result[0].names[i]} vector")
+        cv2.rectangle(original_image,  (int(result[0].boxes.xyxy[i][0]), int(result[0].boxes.xyxy[i][1])), (int(result[0].boxes.xyxy[i][2]), int(result[0].boxes.xyxy[i][3])), (0,255,0), 1)
+        cv2.imshow(f"predicted {result[0].names[i]} image", original_image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        
+
 
 
 if __name__ == "__main__":
@@ -26,23 +41,25 @@ if __name__ == "__main__":
     model = YOLO("best.pt")
 
     # getting predition on specified image path
-    image_path="dataset/img_0.jpg"
+    image_path="dataset/img_1.jpg"
     result = make_prediction(image_path, model)
 
-    #extracting the head and body class cordinates
-    head, body = get_head_body_cordinates(result)
-   
-
-    #displaying the orginal image ---> passing the original image
-    show_img_from_path(image_path)
-
-    # #displaying head prediction image
-    # show_head_predicted_image(original_image_path= image_path, head_cordinates=head)
+    display_head_body(image_path, result)
     
+
+    # #displaying the orginal image ---> passing the original image
+    # show_img_from_path(image_path)
+
+    # #displaying head and body prediction image
+    # show_head_predicted_image(original_image_path= image_path, cordinates=head)
+    # show_head_predicted_image(original_image_path=image_path,cordinates=body )
     
-    #displaying body prediction image
-    original_image =cv2.imread(image_path)
-    predicted_image = cv2.rectangle(original_image,  (int(head[0]), int(head[1])), (int(head[2]), int(head[3])), (0,255,0), 1)
-    cv2.imshow("body prediction", predicted_image)
-    cv2.waitKey()
+    # # #displaying body prediction image
+    # # original_image =cv2.imread(image_path)
+    # # predicted_image = cv2.rectangle(original_image,  (int(body[0]), int(body[1])), (int(body[2]), int(body[3])), (0,255,0), 1)
+    # # cv2.imshow("body prediction", predicted_image)
+    # # cv2.waitKey()
+
+
+
 
