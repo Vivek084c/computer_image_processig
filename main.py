979048297d378c4d704925fc7ca1f66b523760a0
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-07-26 23:31:04 +0530

    update code

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
-from YOLO_Model import head_body_detection_model
-from ultralytics import YOLO
-import cv2
-import numpy as np
-
+from Models.models import build_unet
 
 
 if __name__ == "__main__":
-    #input file path
-    input_image_path = "data/input/img_17.jpg"
-    input_mask_path = "data/Categories_1/img_17.png"
-    model_path = "YOLO_Model/best.pt"
-
-    #defining a model class for head and body detection
-    model_1 = head_body_detection_model.HeadBodyClassifier(input_img_path=input_image_path, input_mask_path=input_mask_path, model_path = model_path)
-
-    
-    #getting the head and body vector
-    out_image_file, out_mask_fiile = model_1.get_head_body_vector()
-
-    #extracting head and body vector
-    head_img = out_image_file["head"]
-    body_img = out_image_file["body"]
-
-    # displaying the head and body vector
-    model_1.show_img(head_img)
-    model_1.show_img(body_img)
-
-    #iextracting head and body mask vector
-    head_img_mask = out_mask_fiile["head"]
-    body_img_mask = out_mask_fiile["body"]
-
-    #displaying the mask
-    model_1.show_img(head_img_mask)
-    model_1.show_img(body_img_mask)
-
-
-  
-
-    
-
-
-
+    input_shape = (512, 512, 3)
+    NUM_CLASSES = 18
+    model = build_unet(input_shape, num_classes=NUM_CLASSES)
+    model.summary()
