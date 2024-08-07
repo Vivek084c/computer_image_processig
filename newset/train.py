4abf65265818bbeb7c008ef99e6bce3d612848a2
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-08-07 22:30:35 +0530

    done model bulding and hyperparameter

--- newset/train.py
+++ newset/train.py
@@ -1,6 +1,6 @@
 # important modules
 import os
-import glob as glb
+import glob 
 import cv2
 import numpy as np
 import tensorflow as tf
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split
 #model import 
 import tensorflow as tf
 from tensorflow.keras import layers, models
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, CSVLogger
+
 
 
 
@@ -28,8 +30,8 @@ def create_dir(path):
         os.makedirs(path)
 
 def load_dataset(path, split=0.2):
-    train_x = sorted(glb(os.path.join(path, "input",  "*")))[:5000]
-    train_y = sorted(glb(os.path.join(path, "Categories_1",  "*")))[:5000]
+    train_x = sorted(glob.glob(os.path.join(path, "input",  "*")))[:5000]
+    train_y = sorted(glob.glob(os.path.join(path, "Categories_1",  "*")))[:5000]
 
     split_size = int(split * len(train_x))
 
@@ -132,64 +134,76 @@ def build_fcn16(input_shape=(512, 512, 3), num_classes=18):
 
 
 if __name__ == "__main__":
-    #seading the environment
-    # np.random.seed(41)
-    # tf.random.set_seed(42)
+    # seading the environment
+    np.random.seed(41)
+    tf.random.set_seed(42)
 
-    # #creating the directory
-    # create_dir(os.path.join("newset", "files"))
+    #creating the directory
+    create_dir(os.path.join("newset", "files"))
 
-    # #hyperparameter
-    # IMG_H = 512
-    # IMG_W = 512
-    # NUM_CLASSES = 18
-    # input_shape = (IMG_H, IMG_W, 3)
-    # batch_size = 10
-    # lr = 1e-4
-    # num_epoch = 30
+    #hyperparameter
+    IMG_H = 512
+    IMG_W = 512
+    NUM_CLASSES = 18
+    input_shape = (IMG_H, IMG_W, 3)
+    batch_size = 10
+    lr = 1e-4
+    num_epoch = 30
 
     #important paths
     dataset_path = "data/"
     model_path = os.path.join("newset","files", "model_head.keras")
     csv_path = os.path.join("newset","files", "data_head.csv")
-
-    print(os.getcwd())
-    l = os.path.join(dataset_path, "input","*")
-    for k in l:
-        print(k)
          
 
-    # # Loading the dataset
-    # (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_dataset(dataset_path)
+    # Loading the dataset
+    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_dataset(dataset_path)
 
 
-    #  # Colormap processing 
-    # COLORMAP = [
-    #     [0, 0, 0], [0, 0, 128], [0, 0, 255], [0, 85, 0], [51, 0, 170],
-    #     [0, 85, 255], [85, 0, 0], [221, 119, 0], [0, 85, 85], [85, 85, 0],
-    #     [0, 51, 85], [128, 86, 52], [0, 28, 0], [255, 0, 0], [221, 170, 51],
-    #     [225, 225, 0], [170, 255, 85], [85, 255, 170]
-    # ]
-    # CLASSES = [
-    #     "Background", "Hat", "Hair", "Sunglasses", "Upper-clothes", "Skirt",
-    #     "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Face", "Left-leg",
-    #     "Right-leg", "Left-arm", "Right-arm", "Bag", "Scarf"
-    # ]
+     # Colormap processing 
+    COLORMAP = [
+        [0, 0, 0], [0, 0, 128], [0, 0, 255], [0, 85, 0], [51, 0, 170],
+        [0, 85, 255], [85, 0, 0], [221, 119, 0], [0, 85, 85], [85, 85, 0],
+        [0, 51, 85], [128, 86, 52], [0, 28, 0], [255, 0, 0], [221, 170, 51],
+        [225, 225, 0], [170, 255, 85], [85, 255, 170]
+    ]
+    CLASSES = [
+        "Background", "Hat", "Hair", "Sunglasses", "Upper-clothes", "Skirt",
+        "Pants", "Dress", "Belt", "Left-shoe", "Right-shoe", "Face", "Left-leg",
+        "Right-leg", "Left-arm", "Right-arm", "Bag", "Scarf"
+    ]
 
-    # # Dataset pipeline
-    # train_dataset = tf_dataset(train_x, train_y, batch=batch_size)
-    # valid_dataset = tf_dataset(valid_x, valid_y, batch=batch_size)
-
-
-
-    # # Model
-    # model = fcn16(input_shape=input_shape, num_classes=NUM_CLASSES)
-    # # sdg optimizer
-    # sgd = tf.keras.optimizers.SGD(learning_rate=lr, momentum=0.9, decay=0.0005)
+    # Dataset pipeline
+    train_dataset = tf_dataset(train_x, train_y, batch=batch_size)
+    valid_dataset = tf_dataset(valid_x, valid_y, batch=batch_size)
 
 
-    # model.compile(
-    #     loss="categorical_crossentropy",
-    #     optimizer=sgd
-    # )
-    # model.summary()
+
+    # Model
+    model = build_fcn16(input_shape=input_shape, num_classes=NUM_CLASSES)
+    # sdg optimizer
+    sgd = tf.keras.optimizers.SGD(learning_rate=lr, momentum=0.9, decay=0.0005)
+
+
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=sgd
+    )
+    model.summary()
+
+    # Training
+    callbacks = [
+        ModelCheckpoint(model_path, verbose=1, save_best_only=True),
+        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+        CSVLogger(csv_path, append=True),
+        EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=False)
+    ]
+
+    print("starting the training proces--------------------------")
+    model.fit(
+        train_dataset,
+        validation_data=valid_dataset,
+        epochs=num_epoch,
+        callbacks=callbacks
+    )
+
